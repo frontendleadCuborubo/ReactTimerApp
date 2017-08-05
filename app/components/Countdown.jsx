50586ceb7ddded7c2f1c718ea53ad5b9b250ba03
frontendleadCuborubo <frontendlead@cuborubo.com>
2017-08-05 18:32:07 +0300

    Initital commit

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Countdown extends React.Component{
+
+  constructor() {
+    super()
+    //this.state = { width: 0 }
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>Countdown</h1>
+      </div>
+    )
+  };
+
+}
+
+module.exports = Countdown;
---
+++ app/components/Timer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Timer extends React.Component{
+
+  constructor() {
+    super()
+    //this.state = { width: 0 }
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>Timer</h1>
+      </div>
+    )
+  };
+
+}
+
+module.exports = Timer;
